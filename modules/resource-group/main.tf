7d9ed935c778196c0da1b71f8d029d3da5338263
chut-th <chutchawin.th@outlook.com>
2024-05-25 11:14:10 +0700

    Update resource group module

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -1,6 +1,10 @@
+locals {
+  name = "${var.csp}${var.service}${var.company}${var.project}${var.environment}${var.running_number}"
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.name
-  location = var.location
+  name       = local.name
+  location   = var.location
   managed_by = var.managed_by
-  tags = var.tags
+  tags       = var.tags
 }
--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -1,20 +1,53 @@
+variable "csp" {
+  description = "The name of cloud service provider"
+  type        = string
+  default     = "az"
+}
+
+variable "service" {
+  description = "The name of the provissioned service"
+  type        = string
+
+}
+
+variable "company" {
+  description = "The company that own the service"
+  type        = string
+}
+
+variable "project" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "running_number" {
+  description = "The running number of the service"
+  type        = string
+}
+
+
 variable "name" {
-    description = "The Name which should be used for this Resource Group."
-    type = string
+  description = "The Name which should be used for this Resource Group."
+  type        = string
 }
 
 variable "location" {
   description = "The Azure Region where the Resource Group should exist."
-  type = string
+  type        = string
 }
 
 variable "managed_by" {
   description = "(Optional) The ID of the resource or application that manages this Resource Group."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "tags" {
   description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
-  default = null
+  default     = null
 }
